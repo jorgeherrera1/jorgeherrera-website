8c4005d08f140de084862347be5d8663392ab6d9
Jorge <jorge.herrera.cabrera@gmail.com>
2022-12-17 09:58:25 -0600

    Changed font to JetBrains Mono

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,10 +3,21 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
+import * as React from "react"
 
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: `en` })
+  setHeadComponents([
+    <link
+      rel="preload"
+      href="/fonts/JetBrainsMono-Regular.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+  ])
 }
